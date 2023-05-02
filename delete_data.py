73d0aa7b65bd1b5fd94125c0c0939c99a567d13f
Kenan <krmlknn@gmail.com>
2023-05-02 18:34:59 +0400

    FINAL

--- delete_data.py
+++ delete_data.py
@@ -3,7 +3,7 @@ from subprocess import call
 
 
 # call(['sudo', 'fuser', '-k', '8000/tcp'])
-call(["rm", "-rf" , "*/migrations/0*"])
+#call(["rm", "-rf" , "*/migrations/0*"])
 # call(["docker", "compose" , "up" , "-d"])
 # call(["./manage.py", "makemigrations"])
 # call(["./manage.py", "migrate"])
